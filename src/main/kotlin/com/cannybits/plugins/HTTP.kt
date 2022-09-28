07fbdbbd3c08d5f1c58c497345851dd86569f1d4
Nyandago <paulmutator@gmail.com>
2022-09-28 12:25:31 +0300

    HTTP.kt

--- src/main/kotlin/com/cannybits/plugins/HTTP.kt
+++ src/main/kotlin/com/cannybits/plugins/HTTP.kt
@@ -2,8 +2,7 @@ package com.cannybits.plugins
 
 import io.ktor.server.plugins.defaultheaders.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+
 
 fun Application.configureHTTP() {
     install(DefaultHeaders) {
