18f8245f2f37f539ef6449a8dd2fe52e5ab779b3
Nyandago <paulmutator@gmail.com>
2022-09-28 17:01:28 +0300

    HeroRepository.kt

---
+++ src/main/kotlin/com/cannybits/repository/HeroRepository.kt
@@ -0,0 +1,20 @@
+package com.cannybits.repository
+
+import com.cannybits.models.ApiResponse
+import com.cannybits.models.Hero
+
+
+interface HeroRepository {
+
+    val heroes: Map<Int, List<Hero>>  //int for page, list for heroes in the page
+
+    val page1: List<Hero>
+    val page2: List<Hero>
+    val page3: List<Hero>
+    val page4: List<Hero>
+    val page5: List<Hero>
+
+
+    suspend fun getAllHeroes(page: Int = 1): ApiResponse
+    suspend fun searchHero(name: String): ApiResponse
+}
