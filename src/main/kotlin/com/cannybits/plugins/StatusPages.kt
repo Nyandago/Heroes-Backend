e71cee39d25ecdaccba12b09e4215eb458edfced
Nyandago <paulmutator@gmail.com>
2022-09-29 13:01:33 +0300

    StatusPages.kt

---
+++ src/main/kotlin/com/cannybits/plugins/StatusPages.kt
@@ -0,0 +1,18 @@
+package com.cannybits.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+
+
+fun Application.configureStatusPages(){
+    install(StatusPages){
+      status(HttpStatusCode.NotFound){ call, status ->
+          call.respond(
+                message = "Page not Found",
+                status = status
+          )
+      }
+    }
+}
